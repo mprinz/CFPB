8f19f912265bc02c770e27d74317f2aa5b3ce305
Michael Prinz <mnprinz@gmail.com>
2016-01-14 16:54:22 -0700

    Created directory for Data Design Project

--- tellStory.php
+++ tellStory.php
@@ -1,10 +1,27 @@
 <!DOCTYPE html>
 	<html>
 	<head>
-		<meta> charset="utf-8">
+		<meta charset="utf-8">
+			<title> Data Design Project</title>
 	</head>
 	<body>
-		<h1>CFPB Tell Your Story Data Design Project</h1>
-			<p>This project involves modeling the Entity Relationship Diagram for the website of the Consumer Financial Protection Agency ("CFPB")</p>
+		<h1>Data Design Project</h1>
+			<p>This project involves modeling the Entity Relationship Diagram for the "Tell Your Story" section of the Consumer Financial Protection Agency ("CFPB") website.
+			This website allows consumers to tell the CFPB about their experience with financial services or products.</p>
+		<h2>Persona</h2>
+			<p>The user of this website is a consumer of financial services or products from a bank, credit union or other financial company.
+			The consumer enters her story in a field on the website.  Consumers are instructed to "describe what happened" but to <em>not</em> enter any sensitive information.
+			Immediately below the story field is a field in which they are directed to "tag" the issue i.e. mortgage, credit card, student loan, etc. This input is not required however.</p>
+
+			<p>The user then has the option of entering the following information:</p>
+				<ul>	<li>email</li>
+						<li>zip code</li>
+						<li>first name</li>
+						<li>last name</li>
+						<li>phone number</li>
+				</ul>
+			<p>Next to this list is a checkbox labeled "Submit Anonymously"
+			Finally, the consumer is required to check "yes" or "no" as to whether they worked for a financial company at the time of the experience</p>
+
 	</body>
 </html>
