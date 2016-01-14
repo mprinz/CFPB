59380ff204167d6b01cd1b29fd20d1a863001d87
Michael Prinz <mnprinz@gmail.com>
2016-01-14 15:29:00 -0700

    Created directory for Data Design Project

---
+++ tellStory.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+	<html>
+	<head>
+		<meta> charset="utf-8">
+	</head>
+	<body>
+		<h1>CFPB Tell Your Story Data Design Project</h1>
+			<p>This project involves modeling the Entity Relationship Diagram for the website of the Consumer Financial Protection Agency ("CFPB")</p>
+	</body>
+</html>
