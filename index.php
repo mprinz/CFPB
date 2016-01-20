0139b2280c497b7c8d0f4069b8b224f5b536bb3e
Michael Prinz <mnprinz@gmail.com>
2016-01-20 11:58:57 -0700

    Created sql file

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+	<html>
+	<head>
+		<meta charset="utf-8">
+
+		<title> CFPB Data Design Project</title>
+	</head>
+	<body>
+		<h1>CFPB Data Design Project</h1>
+			<p>This project involves designing the database needed for the "Tell Your Story" section of the Consumer Financial Protection Agency ("CFPB") website.
+			The website's purpose is to give consumers the opportunity to tell the CFPB about their experience with a business that the agency regulates.  The CFPB will review the submitted information and then use that information to better understand the practices of the financial services industry in order to better regulate this industry.</p>
+		<h2>Persona</h2>
+			<p>	The user of this website is a consumer of financial services or products from a bank, credit reporting agency or other financial company. This is a large portion of the adult population and the user could have any education level or IT knowledge. Thus, the site must be fairly simple to understand and navigate.
+					A typical user is Jane Doe, 32 years old, single mother of two children.  She is a high school graduate and works at a restaurant.  Jane's car needed repairs but she didn't have enough money to afford the repairs.  Jane went to ABC Loan Co. and borrowed money.
+					The interest rate was very high and it took a long time for Jane to pay back the money.  This was a negative experience for Jane.  She repaired her car but had unpleasant experiences with ABC and paid ABC thousands of dollars in interest and late fees.
+		<h2> Use Case</h2>
+			<p>The consumer posts a story by accessing the CFPB web page, writing the story in the appropriate box and clicking the "Tell Your Story" button at the bottom of the page. <a href="https://help.consumerfinance.gov/app/tellyourstory">  Visit CFPB web site </a>
+			<br></br>
+				<img src="images/erDiagram.svg" alt="ER Diagram">
+<!--		<h3>Primary Keys</h3>
+		Consumers are instructed to "describe what happened" but to <em>not</em> enter any sensitive information.
+			Immediately below the story field is a field in which they are directed to "tag" the issue i.e. mortgage, credit card, student loan, etc. This input is not required however.</p>
+
+			<p>The user then has the option of entering the following information:</p>
+				<ul>
+					<li>Email</li>
+					<li>Zip code</li>
+					<li>First name</li>
+					<li>Last name</li>
+					<li>Phone number</li>
+				</ul>
+			<p>Next to this list is a checkbox labeled "Submit Anonymously"
+			Finally, the consumer is required to check "yes" or "no" as to whether they worked for a financial company at the time of the experience</p> -->
+
+	</body>
+</html>
